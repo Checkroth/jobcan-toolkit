35f6b3aab17c9871d86aa27715b83720ec3c98a6
checkroth <checkroth@gmail.com>
2020-12-17 08:30:41 +0900

    v 0.2 update for jobcan design change

--- app/ui-toolkit.js
+++ app/ui-toolkit.js
@@ -3,23 +3,31 @@
 
 
 // working days in the month
-var user_info = Array.prototype.map.call(document.querySelectorAll('table.left')[0].querySelectorAll('td'),
-																											 function (td) {
-																												 return td.innerHTML.trim();
-																											 });
+var user_info = Array.prototype.map.call(
+    document.querySelectorAll('table.jbc-table')[1].querySelectorAll('td'),
+		function (td) {
+				return td.innerHTML.trim();
+		}
+);
 // (0)working days, weekends worked, holidays worked, absences, late clock-ins, early leaves
-var working_days_info = Array.prototype.map.call(document.querySelectorAll('table.right')[0].querySelectorAll('td'),
-																								 function(td) {
-																									 inner = td.innerHTML.trim();
-																									 return parseInt(inner) });
+var working_days_info = Array.prototype.map.call(
+    document.querySelectorAll('table.jbc-table')[2].querySelectorAll('td'),
+		function(td) {
+				inner = td.querySelector('span').innerHTML.trim();
+				return parseInt(inner)
+    }
+);
 // (0)working minutes, scheduled minutes, overtime, night shift, weekday hours, weekday overtime, weekday nightshift,
 //     holiday hours, holiday overtime, holiday nightshift
-var working_minutes_info = Array.prototype.map.call(document.querySelectorAll('table.left')[1].querySelectorAll('td'),
-																										function(td) {
-																											inner = td.innerHTML.trim();
-																											// Take HH:MM format and convert to just minutes
-																											split = inner.split(':');
-																											return (parseInt(split[0]) * 60) + parseInt(split[1]) });
+var working_minutes_info = Array.prototype.map.call(
+    document.querySelectorAll('table.jbc-table')[3].querySelectorAll('td'),
+		function(td) {
+				inner = td.querySelector('span').innerHTML.trim();
+				// Take HH:MM format and convert to just minutes
+				split = inner.split(':');
+				return (parseInt(split[0]) * 60) + parseInt(split[1])
+    }
+);
 
 function minutes_to_hhmm(minutes){
 	hours = parseInt(minutes / 60)
@@ -49,41 +57,46 @@ else {
 }
 
 var table = `
-<table class='left'>
-  <thead>
-    <tr><th colspan=2>Monthly Progress for 7.5 Hour Days</th></tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th>Days in Month</th>
-      <td>${prescribed_working_days}</td>
-    </tr>
-    <tr>
-      <th>Expected hours for month</th>
-      <td>${minutes_to_hhmm(expected_monthly_minutes)}</td>
-    </tr>
-    <tr>
-      <th>Days Worked</th>
-      <td>${worked_days}</td>
-    </tr>
-    <tr>
-      <th>Expected hours worked so far</th>
-      <td>${minutes_to_hhmm(expected_worked_minutes)}</td>
-    </tr>
-    <tr>
-      <th>Actual hours worked</th>
-      <td>${minutes_to_hhmm(working_minutes)}</td>
-    </tr>
-    <tr>
-      <th>Hours Ahead</th>
-      <td>${minutes_to_hhmm(minutes_ahead)}</td>
-    </tr>
-      <th>Hours Behind</th>
-      <td>${minutes_to_hhmm(minutes_behind)}</td>
-    </tr>
-  </tbody>
-</table>
+<div class="col-lg-6 mb-3">
+  <div class="card jbc-card-bordered h-100">
+    <div class="card-header jbc-card-header"><h5 class="card-text">Monthly Progress for 7.5 Hour Days</h5></div>
+    <div class="card-body">
+      <table class='table jbc-table jbc-table-fixed'>
+        <tbody>
+          <tr>
+            <th scope="row" class="jbc-text-sub text-nowrap">Days in Month</th>
+            <td>${prescribed_working_days}</td>
+          </tr>
+          <tr>
+            <th scope="row" class="jbc-text-sub text-nowrap">Expected hours for month</th>
+            <td>${minutes_to_hhmm(expected_monthly_minutes)}</td>
+          </tr>
+          <tr>
+            <th scope="row" class="jbc-text-sub text-nowrap">Days Worked</th>
+            <td>${worked_days}</td>
+          </tr>
+          <tr>
+            <th scope="row" class="jbc-text-sub text-nowrap">Expected hours worked so far</th>
+            <td>${minutes_to_hhmm(expected_worked_minutes)}</td>
+          </tr>
+          <tr>
+            <th scope="row" class="jbc-text-sub text-nowrap">Actual hours worked</th>
+            <td>${minutes_to_hhmm(working_minutes)}</td>
+          </tr>
+          <tr>
+            <th scope="row" class="jbc-text-sub text-nowrap">Hours Ahead</th>
+            <td>${minutes_to_hhmm(minutes_ahead)}</td>
+          </tr>
+            <th scope="row" class="jbc-text-sub text-nowrap">Hours Behind</th>
+            <td>${minutes_to_hhmm(minutes_behind)}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
+  
 `
 
-var append_spot = document.getElementsByClassName('infotpl')[0]
+var append_spot = document.getElementsByClassName('row')[2]
 append_spot.innerHTML = table + append_spot.innerHTML
